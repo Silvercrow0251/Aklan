3773cb479efc93b20f64f14188794825f4e5dd1c
Jonno <77614961+Silvercrow0251@users.noreply.github.com>
2021-11-02 20:20:43 +0800

    Update form-hander.php

--- form-hander.php
+++ form-hander.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject= $_POST['subject'];
 $message= $_POST['message'];
 
-$email_from ='info@silvercrow0251.github.io/Aklan/'
+$email_from ='info@silvercrow0251.github.io'
 
 $email_subject ='New Form Submission'
 
