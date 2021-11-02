4d3d5bca38954b4488d82de9caa8622201ed4d3d
Jonno <77614961+Silvercrow0251@users.noreply.github.com>
2021-11-02 20:53:01 +0800

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject= $_POST['subject'];
 $message= $_POST['message'];
 
-$email_from ='info@silvercrow0251.github.io';
+$email_from ='travel-2-aklan.com.ph';
 
 $email_subject ='New Form Submission';
 
